1a4f3a28aee3deedcd1b2cab6413780f68afee31
troll <trino@mail.ru>
2024-01-22 22:29:41 +0300

    проверка через Зод

--- src/app/panel/author/action/create-author.ts
+++ src/app/panel/author/action/create-author.ts
@@ -1,19 +1,29 @@
 'use server'
 import prisma from "@/app/services/db"
+import { z } from "zod"
+
+const createAuthorSchem = z.object({
+  name: z.string().min(2)
+})
 
 export default async function createAuthor (data: any) {
   try {
     const name = data.get('name')
-    console.log(name)
+
+    const authorData = {
+      name: name
+    } 
+    const validAutor = createAuthorSchem.parse(authorData)
 
     const author = await prisma.authors.create({
-      data: {
-        name: name
-      }
-    }
-    )
+      data: validAutor
+    })
     return (author)
   } catch(error: any) {
+    if (error instanceof z.ZodError) {
+      console.log(error.errors[0].message)
+      return (error.errors[0].message)
+    }
     console.log("Не удалось создать автора")
     return(JSON.stringify(error))
   } 
