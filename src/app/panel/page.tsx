4e13773c6e07e8024dbe36689ed4c6f1cb1344cb
troll <trino@mail.ru>
2024-01-31 00:04:11 +0300

    Отрисовка патриарха и выход

--- src/app/panel/page.tsx
+++ src/app/panel/page.tsx
@@ -2,15 +2,28 @@
 import Link from "next/link";
 import Button from "../shared/Button";
 import AuthPatriarch from "../shared/auth-form";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const Panel = () => {
   const [patriarch, setPatriarch] = useState<boolean>(false)
   
+  const handleOut = () => {
+    localStorage.setItem('privet', '')
+    window.location.reload()
+  }
+
+  // вход в админку
+  useEffect(() => {
+    const initialRole = typeof window !== undefined ? localStorage.getItem('privet') : null
+    if (initialRole === 'Patriarch') {
+      setPatriarch(true)
+    }
+  }, [])
+  
   return (
     <>
-      <AuthPatriarch />
+      {!patriarch && (<AuthPatriarch />)}
       {patriarch && (
         <>
           <h2 className="text-2xl text-center font-semibold mb-4">Создать новый экземпляр</h2>
@@ -30,9 +43,10 @@ const Panel = () => {
             <Link href={'/panel/series'} className="mb-4" >
                 <Button>Серии</Button>
             </Link>
-            <Link href={'/panel/genres'} >
+            <Link href={'/panel/genres'} className="mb-4" >
                 <Button>Жанров</Button>
             </Link>
+            <Button onClick={handleOut}>Выход</Button>
           </div>
         </>
       )}
