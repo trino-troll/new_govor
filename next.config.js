f0abcd3b9ad7e81ca307b9d62d9089119ce849a2
troll <trino@mail.ru>
2024-02-17 23:49:34 +0300

    Измененние настроек некс-конфиг

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/audiobooks/:slug*',
+        destination: '/audiobooks/:slug*', // Путь к папке public, где хранятся файлы
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
