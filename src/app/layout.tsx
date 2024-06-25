1af5a6de523bb41132077f5ef7434dc2d153cf3d
troll <trino@mail.ru>
2024-06-25 23:39:43 +0300

    Google Analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import React from 'react'
+import { Slide, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Footer from './footer/page'
 import './globals.css'
 import Header from './header/page'
-import React from 'react'
-import { ToastContainer, toast, Slide } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
 import MainBanner from './shared/banner/main-banner'
-import Footer from './footer/page'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: {
     default: 'ChatterBox',
-    template: '%s | ChatterBox'
+    template: '%s | ChatterBox',
   },
   description: 'Аудиокниги для меня.',
 }
@@ -25,6 +25,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALITICS}`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', ${process.env.GOOGLE_ANALITICS});
+            `,
+          }}
+        />
+      </head>
       <body className={`${inter.className} bg-black`}>
         <div className="relative min-h-screen max-w-[450px] bg-[#FAFAFA] mx-auto">
           <Header />
